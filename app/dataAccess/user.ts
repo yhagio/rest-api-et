46d8ead8115f0c494713168a8d8332fa9790e591
Yuichi Hagio <yhagio87@gmail.com>
2018-05-11 19:16:40 -0400

    OO -> Functional (model and dataaccess)

--- app/dataAccess/user.ts
+++ app/dataAccess/user.ts
@@ -1,16 +1,19 @@
-export default class UserDB {
-  public static findOne(trx, userId: string) {
-    return trx
-      .select('*')
-      .from('users')
-      .where({ user_id: userId })
-      .first();
-  }
+export default {
+  findOne,
+  all
+};
 
-  public static all(trx, limit = 100) {
-    return trx
-      .select('*')
-      .from('users')
-      .limit(limit);
-  }
+function findOne(trx, userId: string) {
+  return trx
+    .select('*')
+    .from('users')
+    .where({ user_id: userId })
+    .first();
+}
+
+function all(trx, limit = 100) {
+  return trx
+    .select('*')
+    .from('users')
+    .limit(limit);
 }
--- app/models/user.ts
+++ app/models/user.ts
@@ -1,23 +1,26 @@
 import UserDB from '../dataAccess/user';
 
-export default class User {
-  public static async findOne(trx, userId: string): Promise<any> {
-    if (!userId) {
-      throw { error: 'userId is required' };
-    }
+export default {
+  all,
+  findOne
+};
 
-    if (userId.length !== 36) {
-      throw { error: 'Valid userId is required' };
-    }
-
-    const user = await UserDB.findOne(trx, userId);
-    if (!user) {
-      throw { error: 'User not found.' };
-    }
-    return user;
+async function findOne(trx, userId: string): Promise<any> {
+  if (!userId) {
+    throw { error: 'userId is required' };
   }
 
-  public static async all(trx, limit: number): Promise<any> {
-    return UserDB.all(trx, limit);
+  if (userId.length !== 36) {
+    throw { error: 'Valid userId is required' };
   }
+
+  const user = await UserDB.findOne(trx, userId);
+  if (!user) {
+    throw { error: 'User not found.' };
+  }
+  return user;
+}
+
+async function all(trx, limit: number): Promise<any> {
+  return UserDB.all(trx, limit);
 }
