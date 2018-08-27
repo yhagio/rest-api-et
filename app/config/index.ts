2e8453e705156414200f9dc676c322e350cd35be
Yuichi Hagio <yhagio87@gmail.com>
2018-08-27 19:25:11 -0400

    todo comment

--- app/config/index.ts
+++ app/config/index.ts
@@ -2,4 +2,7 @@ const rawConfig = require('./config.json'); /* tslint:disable-line:no-var-requir
 
 const ENV = process.env.NODE_ENV || 'development';
 
+// TODOs
+// Change JSON to JS/TS and use environmental variables
+// for safety especially for production
 export default rawConfig[ENV];
